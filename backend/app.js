50c915fe493ba6bf4e78595cbadc1dd307e23cad
Evgeniievg <evgeniy.litvinv01@gmail.com>
2023-08-08 21:42:22 +0400

    add 'crash test'

--- backend/app.js
+++ backend/app.js
@@ -49,6 +49,12 @@ app.use(cors({
 app.use('/users', auth, require('./routes/users'));
 app.use('/cards', auth, require('./routes/cards'));
 
+app.get('/crash-test', () => {
+  setTimeout(() => {
+    throw new Error('Сервер сейчас упадёт');
+  }, 0);
+});
+
 app.post('/signin', signinValidation, login);
 
 app.post('/signup', signupValidation, createUser);
